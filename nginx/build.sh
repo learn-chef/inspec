3a9990aee80abd860943953807ad5647d722f5e8
Dan Webb <dan.webb@damacus.io>
2022-02-15 16:29:21 +0000

    Remove build scripts and make docker-compose do all the heavy lifting
    Point the workstation service at the pre-built chefworkstation image on
    Dockerhub
    
    Signed-off-by: Dan Webb <dan.webb@damacus.io>

--- nginx/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker build -t "inspec_nginx" .
-docker tag inspec_nginx $1/inspec_nginx
--- workstation/deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-inspec_ver=$(docker run inspec_workstation inspec --version)
-docker push $1/inspec_workstation
-docker push $1/inspec_workstation:$inspec_ver
